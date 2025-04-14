9676720cd8a35abfd596bab7993432044c5e1764
Abdulrahman1204 <aliabdulrahman1204@gmail.com>
2025-04-14 13:56:58 +0300

    fix routes and controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,67 +1,58 @@
-const asyncHandler = require("express-async-handler")
-const{User,validationUpdateUser, validationLoginAndCreateUser} = require("../models/User")
+const asyncHandler = require("express-async-handler");
+const {
+  User,
+  validationUpdateUser,
+  validationCreateUser,
+} = require("../models/User");
 const jwt = require("jsonwebtoken");
-const { cloudinaryUploadImage } = require('../utils/cloudinary');
-const fs = require('fs');
-const upload  = require("../middlewares/photoUpload");
+const { cloudinaryUploadImage } = require("../utils/cloudinary");
+const fs = require("fs");
+const upload = require("../middlewares/photoUpload");
 
 /**
- * @desc Create User 
+ * @desc Create User
  * @route /api/captal/users
  * @method POST
  * @access private
  */
 
 const createUser = [
-    upload,
-    asyncHandler(async (req, res) => {
-    const { error } = validationLoginAndCreateUser(req.body);
-        if (error) {
-        if (req.file?.path) fs.unlinkSync(req.file.path);
-        return res.status(400).json({ message: error.details[0].message });
-        }
-
-    const {
-        email,
-        phone,
-        firstName,
-        lastName,
-        companyName,
-        DateOfCompany,
-        role
-    } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-        if (req.file?.path) fs.unlinkSync(req.file.path);
-        return res.status(409).json({ message: 'البريد الإلكتروني مستخدم من قبل' });
+  upload,
+  asyncHandler(async (req, res) => {
+    const { error } = validationCreateUser(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
     }
 
-    let uploadedFile = null;
-    if (req.file) {
-        const cloudResult = await cloudinaryUploadImage(req.file.path);
-        fs.unlinkSync(req.file.path);
-
-        if (!cloudResult.success) {
-        return res.status(500).json({ message: 'فشل رفع الصورة', error: cloudResult.message });
-        }
-
-        uploadedFile = cloudResult.data;
+    const {
+      email,
+      phone,
+      firstName,
+      lastName,
+      companyName,
+      DateOfCompany,
+      role,
+    } = req.body;
+    const existingUser = await User.findOne({ phone });
+    if (existingUser) {
+      return res
+        .status(409)
+        .json({ message: "البريد الإلكتروني مستخدم من قبل" });
     }
 
     const user = await User.create({
-        email,
-        phone,
-        firstName,
-        lastName,
-        companyName,
-        DateOfCompany,
-        role,
-        attachedFile: uploadedFile,
+      email,
+      phone,
+      firstName,
+      lastName,
+      companyName,
+      DateOfCompany,
+      role,
     });
 
     res.status(201).json({
-    message: 'تم التسجيل بنجاح',
-    user: {
+      message: "تم إنشاء الحساب بنجاح",
+      user: {
         _id: user._id,
         email: user.email,
         phone: user.phone,
@@ -70,86 +61,87 @@ const createUser = [
         companyName: user.companyName,
         DateOfCompany: user.DateOfCompany,
         role: user.role,
-        attachedFile: uploadedFile,
-        },
+      },
     });
-    }),
+  }),
 ];
 
-
 /**
- * @desc Update User 
+ * @desc Update User
  * @route /api/users/:id
  * @method PUT
  * @access private
  */
 
 const updateUser = asyncHandler(async (req, res) => {
-    const { email, firstName, lastName } = req.body;
-    if(req.user.id != req.params.id){
-        return res.status(403).json({message : "You are not allowed"})
-    }
-    const {error} = validationUpdateUser(req.body)
-    if(error){
-        return res.status(400).json({message : error.details[0].message})
-    }
-    const updateduser = await User.findByIdAndUpdate(req.params.id,{
-        $set : {
-            email,
-            firstName,
-            lastName,
-        }
-    },{new : true})
-    res.status(200).json(updateduser)
-})
+  const { email, firstName, lastName } = req.body;
+  if (req.user.id != req.params.id) {
+    return res.status(403).json({ message: "You are not allowed" });
+  }
+  const { error } = validationUpdateUser(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  const updateduser = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        email,
+        firstName,
+        lastName,
+      },
+    },
+    { new: true }
+  );
+  res.status(200).json(updateduser);
+});
 
 /**
- * @desc Get All User 
+ * @desc Get All User
  * @route /api/users
  * @method GET
  * @access private (Only admin)
  */
-const getAllUsers = asyncHandler(async(req,res)=>{
-    const users = await User.find()
-    res.status(200).json(users)
-})
+const getAllUsers = asyncHandler(async (req, res) => {
+  const users = await User.find();
+  res.status(200).json(users);
+});
 
 /**
- * @desc Get User By ID 
+ * @desc Get User By ID
  * @route /api/users/:id
  * @method GET
  * @access private
  */
-const getUserByID = asyncHandler(async(req,res)=>{
-    const user = await User.findById(req.params.id)
-    if (user) {
-        res.status(200).json(user)
-    } else {
-        res.status(404).json({message : "user not found"})
-    }
-})
+const getUserByID = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).json({ message: "user not found" });
+  }
+});
 
 /**
- * @desc Delete User 
+ * @desc Delete User
  * @route /api/users/:id
  * @method DELETE
- * @access private 
+ * @access private
  */
-const deleteUser = asyncHandler(async(req,res)=>{
-    const user = await User.findById(req.params.id)
-    if (user) {
-        await User.findByIdAndDelete(req.params.id)
-        res.status(200).json({message:"user has been deleted successfully"})
-    } else {
-        res.status(404).json({message : "user not found"})
-    }
-})
-
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "user has been deleted successfully" });
+  } else {
+    res.status(404).json({ message: "user not found" });
+  }
+});
 
 module.exports = {
-    updateUser,
-    getAllUsers,
-    getUserByID,
-    deleteUser,
-    createUser
-}
+  updateUser,
+  getAllUsers,
+  getUserByID,
+  deleteUser,
+  createUser,
+};
