bef5bde19a3ac6c46728d101e408ed66b89ace28
Abdulrahman1204 <aliabdulrahman1204@gmail.com>
2025-04-24 23:13:08 +0300

    updates

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.get("/api/captal/get-cookies", (req, res) => {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () =>
   console.log(`server is running in ${process.env.NODE_ENV} on port ${PORT}`)
 );
