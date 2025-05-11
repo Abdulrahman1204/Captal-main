6fefb9092d222e241f541f2fec5259cef65f8697
Abdulrahman <137169908+Abdulrahman1204@users.noreply.github.com>
2025-05-11 19:20:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,11 +18,7 @@ const app = express();
 //Cors Policy
 app.use(
   cors({
-<<<<<<< HEAD
     origin: ["http://localhost:5173", "https://captalsa.com"],
-=======
-    origin: ["http://localhost:5173","https://captalsa.com"],
->>>>>>> 3eb9d58033d06d219c6bd5ad0581430f0d7e722c
     credentials: true,
   })
 );
@@ -39,7 +35,7 @@ app.use("/api/captal/orderFinance", require("./routes/orderFinance"));
 app.use("/api/captal/user", require("./routes/user"));
 app.use("/api/captal/material", require("./routes/matrials"));
 app.use("/api/captal/orderMaterial", require("./routes/orderMaterial"));
-<<<<<<< HEAD
+
 app.use(
   "/api/captal/classficationMaterial",
   require("./routes/classficationMaterial")
@@ -50,22 +46,16 @@ app.use(
 );
 app.use("/api/captal/recourseUserOrder", require("./routes/recourseUser"));
 
-=======
 app.use("/api/captal/classficationMaterial", require("./routes/classficationMaterial"));
 app.use("/api/captal/classficationMaterialSon", require("./routes/classficationMaterialSon"));
 app.use("/api/captal/recourseUserOrder", require("./routes/recourseUser"));
 
 // Special Route for Cookies
->>>>>>> 3eb9d58033d06d219c6bd5ad0581430f0d7e722c
 app.get("/api/captal/get-cookies", (req, res) => {
   const myCookieToken = req.cookies.token;
 
   try {
     const decoded = jwt.decode(myCookieToken);
-<<<<<<< HEAD
-
-=======
->>>>>>> 3eb9d58033d06d219c6bd5ad0581430f0d7e722c
     const role = decoded?.role;
     const id = decoded?.id;
 
@@ -88,23 +78,12 @@ app.use(errorHandler);
 
 // SSL Certificates
 const sslOptions = {
-<<<<<<< HEAD
-  key: fs.readFileSync("/etc/letsencrypt/live/captalsa.com/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/captalsa.com/fullchain.pem"),
-=======
   key: fs.readFileSync('/etc/letsencrypt/live/captalsa.com/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/captalsa.com/fullchain.pem'),
->>>>>>> 3eb9d58033d06d219c6bd5ad0581430f0d7e722c
 };
 
 // Start HTTPS Server
 const PORT = process.env.PORT || 8000;
 https.createServer(sslOptions, app).listen(PORT, () => {
-<<<<<<< HEAD
-  console.log(
-    `🚀 HTTPS server is running on https://srv719334.hstgr.cloud:${PORT}`
-  );
-=======
   console.log(`🚀 HTTPS server is running on https://srv719334.hstgr.cloud:${PORT}`);
->>>>>>> 3eb9d58033d06d219c6bd5ad0581430f0d7e722c
 });
