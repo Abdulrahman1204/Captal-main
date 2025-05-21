6b88597a3ffe5d6eabb8b72e0fe3835115a202bc
Abdulrahman <137169908+Abdulrahman1204@users.noreply.github.com>
2025-05-21 07:58:44 +0300

    Update OrderFinance.js

--- models/OrderFinance.js
+++ models/OrderFinance.js
@@ -96,7 +96,7 @@ function validationOrderFinance(obj) {
     phone: Joi.string()
       .pattern(/^[0-9]{10}$/)
       .required(),
-    email: Joi.string().email().min(8).max(100).allow(""),
+    email: Joi.string().email().min(5).max(100).allow(""),
     companyName: Joi.string().trim().allow(""),
     dateOfCompany: Joi.string().allow(""),
     projectName: joi.string().trim().allow(""),
